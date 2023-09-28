930641c0ddd800a53573854b65ace7012a4a8ff7
nikakakhiani94 <Nika.kakhiani@elumer.io>
2023-09-28 23:19:02 +0400

    added fonts, dummy data and more.

--- src/containers/domainItems/DomainItems.tsx
+++ src/containers/domainItems/DomainItems.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import DomainSort from '@/components/domainSort/DomainSort';
+import DomainItem from '@/components/domainItem/DomainItem';
 
 export default function DomainItems() {
-  return <DomainSort />;
+  return (
+    <>
+      <DomainSort />
+      <DomainItem />
+    </>
+  );
 }
